e3c98e885e9031a50715a8828f56217ee2d24a48
o.pereyra <onasis.pereyra@gmail.com>
2025-08-14 18:31:55 -0400

    Deleting an unnecessary migration file.

--- SecurePass/SecurePass.Infraestructure/obj/Debug/net8.0/SecurePass.Infrastructure.AssemblyInfo.cs
+++ SecurePass/SecurePass.Infraestructure/obj/Debug/net8.0/SecurePass.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurePass.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7690034fb4e0741eb3d95470c73eba6cab09f639")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be815a3b3c87c0d81224e6ebd817472d46b2f5b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurePass.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurePass.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
